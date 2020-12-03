cdd9b133add2ee624082a26b64fd3bed14233039
Ryan <ryaneaster10@gmail.com>
2020-12-03 15:56:46 +0000

    Comments
    Added Docstring comments to each def()

--- scripts/data_scraper.py
+++ scripts/data_scraper.py
@@ -6,6 +6,13 @@ from csv_manipulator import load_csv, date_time
 from data_validation import data_validation
 
 def get_url():
+    '''
+    Array lists all available currency pairs
+    In a loop the pair is then concat into a specific URL which pulls 10 years worth of data for that currency
+    Time period is 01/01/10 - 01/01/2020
+    Error handling used to check the link is accessible
+    The data is then downloaded into the Data folder an named as the currency pair (x)
+    '''
     currency_pair = ["EURUSD", "EURJPY", "USDCHF", "GBPUSD", "USDJPY", "USDCAD", "AUDUSD", "EURGBP", "EURAUD", "EURCHF", "GBPCHF", "CADJPY", "GBPJPY",
                      "AUDNZD", "AUDCAD", "AUDCHF", "AUDJPY", "CHFJPY", "EURNZD", "EURCAD", "CADCHF", "NZDJPY", "NZDUSD", "GBPAUD", "GBPNZD", "NZDCAD", "NZDCHF"]
     currency_pairs = sorted(currency_pair)
--- scripts/data_validation.py
+++ scripts/data_validation.py
@@ -4,10 +4,18 @@ from pandas import Series, DataFrame
 
 
 def data_validation(x):
+    '''
+    Loads up the data file for the given currency (x)
+    Removes the Volume column as it is always zero
+    Check the file for any missing values
+    If values are missing, the cell is filled with a mean of the value before and after
+    Writes new data frame back to the CSV
+    '''
     data_location = 'Data/' + x + '.csv'
     data = pd.read_csv(data_location)
     data.drop('Volume', axis=1, inplace=True)
     data.columns = ["Date", "Open", "High", "Low", "Close", "Adj Close"]
+    print(data.isnull().sum())
     data = data.interpolate()
     print(data.isnull().sum())
     try:
