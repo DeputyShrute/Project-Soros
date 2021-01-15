ea4670bdd04aa3c14b296b9b5db07cf837f27532
Ryan <ryaneaster10@gmail.com>
2021-01-15 21:38:41 +0000

    Changed window period
    28 Days

--- scripts/Testing/linear.py
+++ scripts/Testing/linear.py
@@ -13,71 +13,81 @@ from numpy.core.fromnumeric import size
 
 
 def split_sequence(sequence, n_steps):
-    X, y = list(), list()
-    for i in range(len(sequence)):
-        # find the end of this pattern
-        end_ix = i + n_steps
-        # check if we are beyond the sequence
-        if end_ix > len(sequence)-1:
-            break
-        # gather input and output parts of the pattern
-        seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
-        X.append(seq_x)
-        y.append(seq_y)
-    return array(X), array(y)
+	X, y = list(), list()
+	for i in range(len(sequence)):
+		# find the end of this pattern
+		end_ix = i + n_steps
+		# check if we are beyond the sequence
+		if end_ix > len(sequence)-1:
+			break
+		# gather input and output parts of the pattern
+		seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+		X.append(seq_x)
+		y.append(seq_y)
+	return array(X), array(y)
 
 
 def data():
-    # define input sequence
-    raw_seq = []
-    with open('/home/ryan/Documents/Python/Project-Soros/scripts/Finance_Data/AUDCAD.csv', 'r') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        for lines in csv_reader:
-            if lines[1] != 'null':
-                raw_seq.append(float(lines[1]))
-    raw_seq.pop(0)  # remove column header
-    model(raw_seq)
+	# define input sequence
+	raw_seq = []
+	with open('/home/ryan/Documents/Python/Project-Soros/scripts/Finance_Data/AUDCAD.csv', 'r') as csv_file:
+		csv_reader = csv.reader(csv_file, delimiter=',')
+		for lines in csv_reader:
+			if lines[1] != 'null':
+				raw_seq.append(float(lines[1]))
+	raw_seq.pop(0)  # remove column header
+	model(raw_seq)
 
-# choose a number of time steps
+
+def split_data(raw_seq, n_steps):
+	# split into samples
+	X, y = split_sequence(raw_seq, n_steps)
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+	return X_train, X_test, y_train, y_test
 
 
 def model(raw_seq):
-    n_steps = 4
-    for i in range(3):
+	n_steps = 27
+	for i in range(3):
 
-        # split into samples
-        X, y = split_sequence(raw_seq, n_steps)
+		X_train, X_test, y_train, y_test = split_data(raw_seq, n_steps)
 
-        # define model
-        model = Sequential()
-        model.add(Dense(800, activation='relu', input_dim=n_steps))
-        model.add(Dense(1))
-        model.compile(optimizer='adam', loss='mse')
-        model.summary()
+		print(size(X_train))
 
-        # fit model
-        model.fit(X, y, epochs=1000, verbose=0)
+		# define model
+		model = Sequential()
+		model.add(Dense(100, activation='relu', input_dim=n_steps))
+		model.add(Dense(1))
+		model.compile(optimizer='adam', loss='mse')
+		model.summary()
 
-        train_results = model.evaluate(X, y, verbose=0)
-        print(f'RMSE TRAIN: {round(np.sqrt(train_results), 2)}')
+		# fit model
+		model.fit(X_train, y_train, epochs=1000, verbose=0)
 
-        # demonstrate prediction
-        x_input = array([0.9570, 0.9497, 0.9493, 0.9495])
-        x_input = x_input.reshape((1, n_steps))
-        yhat = model.predict(x_input, verbose=0)
+		train_results = model.evaluate(X_train, y_train, verbose=0)
+		print(f'RMSE TRAIN: {round(np.sqrt(train_results), 2)}')
 
-        print("----------------")
-        print(yhat)
-        log(yhat)
+		# demonstrate prediction
+		test = array([0.94551, 0.9393, 0.9498, 0.9473, 0.9489, 0.9486, 0.9564, 0.9604, 0.9565, 0.9524, 0.9526, 0.9489, 0.9501, 0.9515, 0.952, 0.9495, 0.9547, 0.9559, 0.9556, 0.9524, 0.9522, 0.946, 0.9463, 0.9376, 0.9362, 0.9307, 0.9271])
+		test = test.reshape((1, n_steps))
+		yhat = model.predict(test, verbose=0)
+
+		print("----------------")
+		print(yhat)
+		log(yhat, i)
 
 
-def log(yhat):
-    outF = open('output.txt', 'a')
-    for i in yhat:
-        outF.write(str(i))
-        outF.write('\n')
-    outF.close()
+def log(yhat, iteration):
+	outF = open('output.txt', 'a')
+	for i in yhat:
+		output = "\nIteration: %d\n" %(iteration)
+		outF.write(str(output))
+		outF.write(str(i).strip("[]"))
+		outF.write('\n')
+		outF.write('------------------------')
+		outF.write('\n')
+	outF.close()
 
 
 if __name__ == "__main__":
-    data()
+	data()
