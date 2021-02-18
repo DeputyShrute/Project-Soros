eb7d066d0faf15bee4af2227b0820bde7df8b116
Ryan <ryaneaster10@gmail.com>
2021-02-18 13:19:47 +0000

    Updated labels processing

--- scripts/CNN_Model.py
+++ scripts/CNN_Model.py
@@ -15,7 +15,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 class Model:
 
     def __init__(self, symbol, timestep, column, loop):
-        print('Constructor Initialsed')
+        print('Constructor Initialised')
         self.symbol = symbol
         self.timestep = timestep
         self.column = column.upper()
@@ -27,16 +27,16 @@ class Model:
             self.column = 4
         self.loop = loop
 
-    def split_sequence(sequence, n_steps):
+    def split_sequence(raw_seq, new_seq, n_steps):
         X, y = list(), list()
-        for i in range(len(sequence)):
+        for i in range(len(raw_seq)):
             # find the end of this pattern
             end_ix = i + n_steps
             # check if we are beyond the sequence
-            if end_ix > len(sequence)-1:
+            if end_ix > len(new_seq)-1:
                 break
             # gather input and output parts of the pattern
-            seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+            seq_x, seq_y = raw_seq[i:end_ix], new_seq[end_ix]
             X.append(seq_x)
             y.append(seq_y)
         return array(X), array(y)
@@ -50,27 +50,37 @@ class Model:
             for lines in csv_reader:
                 if lines[self.column] != 'null':
                     raw_seq.append(float(lines[self.column]))
-        raw_seq.pop(0)  # remove column header
-        Model.model(self, raw_seq)
+        new_seq = []
+        new_seq.append(0)
+        j = len(raw_seq)
+        for i in range(1, j):
+            if raw_seq[i] > raw_seq[i-1]:
+                new_seq.append(1) # UP
+            if raw_seq[i] < raw_seq[i-1]:
+                new_seq.append(0)# DOWN
+            # if raw_seq[i] == raw_seq[i-1]:
+            #     new_seq.append(0) # SIDE
 
-    def split_data(raw_seq, n_steps, size):
+        Model.model(self, raw_seq, new_seq)
+
+    def split_data(raw_seq, new_seq ,n_steps, size):
         # split into samples
-        X, y = Model.split_sequence(raw_seq, n_steps)
+        X, y = Model.split_sequence(raw_seq, new_seq, n_steps)
         X_train, X_test, y_train, y_test = train_test_split(
             X, y, test_size=size)
         return X_train, X_test, y_train, y_test
 
-    def model(self, raw_seq):
+    def model(self, raw_seq, new_seq):
         average = []
         for i in range(self.loop):
 
             # Splits the data into test and train (data, windows, size of test)
             X_train, X_test, y_train, y_test = Model.split_data(
-                raw_seq, self.timestep, 0.2)
+                raw_seq, new_seq, self.timestep, 0.2)
 
             # Splits the data into test and val (data, windows, size of val)
             X_train, X_val, y_train, y_val = Model.split_data(
-                raw_seq, self.timestep, 0.2)
+                raw_seq, new_seq, self.timestep, 0.2)
 
             # Reshapes the data for input dimension
             X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], 1))
@@ -85,12 +95,12 @@ class Model:
             model.add(Flatten())
             model.add(Dense(50, activation='relu'))
             model.add(Dense(1))
-            model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
+            model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
             model.summary()
 
             # fit model
             history = model.fit(
-                X_train, y_train, validation_data=(X_val, y_val), epochs=1000, verbose=2, shuffle=True)
+                X_train, y_train, validation_data=(X_val, y_val), epochs=500, verbose=2, shuffle=True)
 
             # Plot accuracy metrics
             pyplot.title('Loss / Mean Squared Error')
@@ -106,19 +116,20 @@ class Model:
             pyplot.show()
 
 
-            # Test model
+            #Test model
             #X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], 1))
-            # new_seq = array(raw_seq[-27:])
-            # new_seq = new_seq.reshape((1, self.timestep, 1))
-            # yhat = model.predict(new_seq, verbose=0)
-            # # Print and log output
-            # print("----------------")
-            # print(yhat)
-            # average.append(yhat)
-            # Model.log(yhat, i)
+            new_seq = array(raw_seq[-27:])
+            new_seq = new_seq.reshape((1, self.timestep, 1))
+            yhat = model.predict(new_seq, verbose=0)
+            # Print and log output
+            print("----------------")
+            print(yhat)
+            Model.direction(yhat)
+            average.append(yhat)
+            Model.log(yhat, i)
 
-        tot_avg = (sum(average)/self.loop)
-        print(tot_avg)
+        # tot_avg = (sum(average)/self.loop)
+        # print(tot_avg)
 
     def log(yhat, iteration):
         outF = open('output.txt', 'a')
@@ -133,6 +144,11 @@ class Model:
             outF.write('\n')
         outF.close()
 
+    def direction(yhat):
+        if yhat >= 0.5:
+            print('UP')
+        if yhat < 0.5:
+            print('Down')
 
 if __name__ == "__main__":
     Open = Model('AUDCAD', 27, 'open', 1)
