55b33c12c39453df611056b566f021980de853ec
Ryan <ryaneaster10@gmail.com>
2021-02-18 13:09:19 +0000

    Rewrite of CNN

--- in_progress.py
+++ in_progress.py
@@ -5,6 +5,8 @@ from keras.layers import Flatten
 from keras.layers import Dense
 from keras.models import Sequential
 from matplotlib import pyplot
+import pandas as pd
+import numpy as np
 import tensorflow as tf
 from numpy import array
 import csv
@@ -27,65 +29,72 @@ class Model:
             self.column = 4
         self.loop = loop
 
-    def split_sequence(sequence, n_steps):
+    def split_sequence(features, labels, n_steps):
         X, y = list(), list()
-        for i in range(len(sequence)):
+        for i in range(len(features)):
             # find the end of this pattern
             end_ix = i + n_steps
             # check if we are beyond the sequence
-            if end_ix > len(sequence)-1:
+            if end_ix > len(labels)-1:
                 break
             # gather input and output parts of the pattern
-            seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+            seq_x, seq_y = features[i:end_ix], labels[end_ix-2]
             X.append(seq_x)
             y.append(seq_y)
         return array(X), array(y)
-
+    
     def data(self):
-        # define input sequence
-        raw_seq = []
-        new_seq = []
-        with open('C:/Users/Ryan Easter/OneDrive - University of Lincoln/University/Year 4 (Final)/Project/Artefact/Project-Soros/Data/' + self.symbol + '.csv', 'r') as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            next(csv_reader)
-            for lines in csv_reader:
-                if lines[self.column] != 'null':
-                    raw_seq.append(float(lines[self.column]))
-        raw_seq.pop(0)  # remove column header
-        j = len(raw_seq)
-        k=2
-        for i in raw_seq: 
+
+        features = pd.read_csv('Data/'+ self.symbol +'.csv')
+        labels = pd.DataFrame(columns=['Date', 'Direction'])
+
+        j = len(features.index)
+        k = 2
+        for i in features['Open']:
             if k >= j:
                 break
             else:
-                if i > raw_seq[k-1]:
-                    new_seq.append(2) # UP
+                if i > features['Open'][k-1]:
+                    z = features['Date'][k]
+                    labels = labels.append({'Date': z, 'Direction': 2}, ignore_index=True) # UP
                     k=k+1
-                if i < raw_seq[k-1]:
-                    new_seq.append(0) # DOWN
+                if i < features['Open'][k-1]:
+                    z = features['Date'][k]
+                    labels = labels.append({'Date': z, 'Direction': 1}, ignore_index=True)# DOWN
                     k=k+1
-                if i == raw_seq[k-1]:
-                    new_seq.append(1) # SIDE
+                if i == features['Open'][k-1]:
+                    z = features['Date'][k]
+                    labels = labels.append({'Date': z, 'Direction': 0}, ignore_index=True)# SIDE
                     k=k+1
-        Model.model(self, new_seq)
+        
+        features = features.iloc[:,[2]].to_numpy()
+        labels = labels.iloc[:,[0,1]].to_numpy()
 
-    def split_data(raw_seq, n_steps, size):
+        Model.model(self, features, labels)
+
+    def split_data(features, labels, n_steps, size):
         # split into samples
-        X, y = Model.split_sequence(raw_seq, n_steps)
+        X, y = Model.split_sequence(features, labels, n_steps)
+
         X_train, X_test, y_train, y_test = train_test_split(
             X, y, test_size=size)
         return X_train, X_test, y_train, y_test
 
-    def model(self, new_seq):
+    def model(self, features, labels):
         average = []
         for i in range(self.loop):
 
             # Splits the data into test and train (data, windows, size of test)
             X_train, X_test, y_train, y_test = Model.split_data(
-                new_seq, self.timestep, 0.2)
+                features, labels, self.timestep, 0.2)
+
             # Splits the data into test and val (data, windows, size of val)
             X_train, X_val, y_train, y_val = Model.split_data(
-                new_seq, self.timestep, 0.1)
+                features, labels, self.timestep, 0.1)
+
+           # print(y_train[:,1])
+            # X_train = np.asarray(X_train[:, :, 1]).astype('float32')
+            # y_train = np.asarray(y_train[:,1]).astype('float32')
 
             # Reshapes the data for input dimensions
             X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], 1))
@@ -96,7 +105,7 @@ class Model:
             model = Sequential()
             #Conveluted layer
             model.add(Conv1D(filters=64, kernel_size=2,
-                             activation='relu', input_shape=(self.timestep, 1)))
+                             activation='relu', input_shape=(self.timestep,1)))
             model.add(MaxPooling1D(pool_size=2))
             model.add(Flatten())
             model.add(Dense(50, activation='relu'))
